4f778b4f0d6da6ccbfe511db44c17c76a706e160
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-06-03 19:43:04 +0300

    Add files via upload
    add data.js

---
+++ data.js
@@ -0,0 +1,47 @@
+const data = [
+    {
+        name: "Пилососи",
+        products: [
+            {
+                name: "Робот-пилосос Rowenta",
+                description:`Робот-пилосос Rowenta Description`,
+                price: 'ціна: 12999 грн',
+            },
+            {
+                name: "Акумул.пилосос Rowenta X-Force",
+                  description:`Акумул.пилосос Rowenta X-Force Description`,
+                price: 'ціна: 16999 грн',
+            }
+        ],
+    },
+    {
+        name: "Пральні машини",
+        products: [
+            {
+                name: "Пр.маш WHIRLPOOL WRBSB",
+                description:`Пр.машина WHIRLPOOL WRBSB Description`,
+                price: 'ціна: 13999 грн',
+            },
+            {
+                name: "Пральна машина INDESIT",
+                description: `Пральна машина INDESIT Description`,
+                price: 'ціна: 9999 грн',
+            }
+        ],
+    },
+    {
+        name: "Холодильники",
+        products: [
+            {
+                name: "Холодильник SAMSUNG",
+                description: `Холодильник SAMSUNG Description`,
+                price: 'ціна: 26999 грн',
+            },
+            {
+                name: "Холодильник BOSCH ",
+                description: "Холодильник BOSCH Description",
+                price: 'ціна: 15599 грн',
+            }
+        ],
+    },
+]
